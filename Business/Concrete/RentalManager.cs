034b1ac2d37d042e15e087cb7ebcced8e0eba0c5
Soner Çelik <snrclk94@outlook.com>
2021-08-03 12:54:36 +0300

    -Added AOP support.

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,6 +23,7 @@ namespace Business.Concrete
             this._rentalDal = rentalDal;
         }
 
+        [ValidationAspect(typeof(RentalValidator))]
         public IResult Add(Rental rental)
         {
             deneme = _rentalDal.GetAll();
